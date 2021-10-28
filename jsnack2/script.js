c66ada0a945e9a32ce8fd72df491d0834195a674
Tommaso Passarotto <passarotto13@gmail.com>
2021-10-28 15:29:10 +0200

    aggiunta dei prompt

---
+++ jsnack2/script.js
@@ -0,0 +1,12 @@
+// L’utente inserisce due parole in successione, con due prompt.
+// Il software stampa prima la parola più corta, poi la parola più lunga.
+// ...
+// 1. Chiedere all'utente 2 parole => 2xprompt
+// 2. Confronto la lunghezza delle due parole
+// 3. Stampo quella più grande
+// 4. Stampo quella più piccola
+
+const userPar1 = prompt("Scrivi qui la prima parola da confrontare: ");
+console.log(userPar1);
+const userPar2 = prompt("Scrivi qui la seconda parola da confrontare: ");
+console.log(userPar2);
